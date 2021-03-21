01902c4c99176145309576d89a36eac1277929fb
Nanase <nanase@users.noreply.github.com>
2021-03-21 22:47:48 +0900

    タイトルイメージ追加と電源投入直後に選曲ができない問題を修正 (close #1)

--- Src/japariradio/radio.cpp
+++ Src/japariradio/radio.cpp
@@ -25,6 +25,61 @@ Button buttonA, buttonB;
 char buf[32];
 Settings settings(&eep);
 
+extern const CImage image_radio;
+extern const CImage image_rod;
+extern const CImage image_slider;
+extern const CImage image_tab;
+extern const CImage image_title_japari;
+extern const CImage image_title_radio;
+
+void title() {
+  // 1
+  radio.begin(RDA5807M_BAND_WORLD);
+  radio.setVolume(0);
+  radio.setFrequency(settings.getValue());
+  HAL_Delay(1000);
+
+  {
+    // title: Japari
+    ssd1306_setFillMode(true);
+    ssd1306_setCursor(40, 0);
+    cImage_write(&image_title_japari);
+    ssd1306_updateScreen();
+  }
+
+  // 2
+  radio.begin(RDA5807M_BAND_WORLD);
+  radio.setVolume(0);
+  radio.setFrequency(settings.getValue());
+  HAL_Delay(1000);
+
+  {
+    // title: Radio
+    ssd1306_setFillMode(false);
+    ssd1306_setCursor(64, 16);
+    cImage_write(&image_title_radio);
+    ssd1306_updateScreen();
+  }
+
+  // 3
+  radio.begin(RDA5807M_BAND_WORLD);
+  radio.setVolume(0);
+  radio.setFrequency(settings.getValue());
+  HAL_Delay(1000);
+
+  // 4
+  radio.begin(RDA5807M_BAND_WORLD);
+  radio.setVolume(0);
+  radio.setFrequency(settings.getValue());
+  HAL_Delay(1000);
+
+  // 5
+  radio.begin(RDA5807M_BAND_WORLD);
+  radio.setVolume(0);
+  radio.setFrequency(settings.getValue());
+  HAL_Delay(1000);
+}
+
 void setup() {
   ssd1306_initialize();
   ssd1306_setContrast(0x7f);
@@ -37,10 +92,31 @@ void setup() {
   HAL_TIM_Base_Start_IT(&htim16);
 #endif
 
+  {
+    // radio
+    ssd1306_setFillMode(true);
+    ssd1306_setCursor(1, 13);
+    cImage_write(&image_radio);
+
+    // rod
+    ssd1306_setCursor(8, 0);
+    cImage_write(&image_rod);
+
+    // slider & tab
+    ssd1306_setFillMode(true);
+    ssd1306_setCursor(4, 25);
+    cImage_write(&image_slider);
+
+    ssd1306_setFillMode(false);
+    ssd1306_setCursor(4 + 9 - (8000 - 7600) / 352, 25);
+    cImage_write(&image_tab);
+    ssd1306_updateScreen();
+  }
+
   settings.prepare();
 
-  radio.begin(RDA5807M_BAND_WORLD);
-  HAL_Delay(50);
+  title();
+
   radio.setFrequency(settings.getValue());
   radio.setVolume(15);
 

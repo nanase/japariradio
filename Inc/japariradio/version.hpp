c0c0a5abad285950488de2dd1d8655176c2b074e
Nanase <nanase@users.noreply.github.com>
2022-04-05 02:13:43 +0900

    バージョン改定

--- Inc/japariradio/version.hpp
+++ Inc/japariradio/version.hpp
@@ -1,8 +1,8 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define SOFTWARE_VERSION "1.1.0"
-#define HARDWARE_VERSION "Rev.1"
+#define SOFTWARE_VERSION "1.2.0"
+#define HARDWARE_VERSION "Rev.3"
 
 #define SUPPORT_SITE "nanase.cc/jprd"
 

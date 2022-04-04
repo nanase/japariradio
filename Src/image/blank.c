02ba4e8503a6d2c302ac2eb380697c2a26144466
Nanase <nanase@users.noreply.github.com>
2022-04-05 02:13:27 +0900

    イメージファイルを統合

--- Src/image/blank.c
+++
@@ -1,6 +0,0 @@
-#include "lib/c_image.h"
-
-static const uint8_t image_blank_data[5] = {
-  0xf0, 0xf0, 0xf0, 0xf0, 0x40,
-};
-const CImage image_blank = { 5, 8, image_blank_data };
--- Src/image/tab.c
+++
@@ -1,6 +0,0 @@
-#include "lib/c_image.h"
-
-static const uint8_t image_tab_data[1] = {
-  0x06,
-};
-const CImage image_tab = { 1, 2, image_tab_data };
